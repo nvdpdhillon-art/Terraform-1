5ef522c551b0d59d9a50bb07ee963215d153cfa0
navdeep <nvdp.dhillon4@veenuindoliagmail.onmicrosoft.com>
2026-06-13 07:20:12 +0000

    used prefix in naming while it was already declared and assigned

--- rg-vnet-subnet/rg/main.tf
+++ rg-vnet-subnet/rg/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rgs" {
     for_each = var.vrgs
-    name = each.value.name
+    name = "${var.vprefix}-${each.value.name}"
     location = each.value.location
     tags = each.value.tags
 
